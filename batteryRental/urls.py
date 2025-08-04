f37d2259ca14d683a4aa467e5eb8585598534db3
gaagnec <gaagnec@gmail.com>
2025-08-04 23:56:43 +0200

    Add files via upload

---
+++ batteryRental/urls.py
@@ -0,0 +1,29 @@
+"""
+URL configuration for batteryRental project.
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/5.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from core.custom_admin import custom_admin_site
+from django.urls import path, include
+from core import views as core_views
+from core.admin import admin_site
+
+urlpatterns = [
+    path('admin_tools/', include('admin_tools.urls')),  # добавляем!
+        path("admin/", custom_admin_site.urls),
+    path('admin/batteries/', core_views.battery_list, name='battery_list'),
+    path('', include('core.urls')),
+]
+
+
